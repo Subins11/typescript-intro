2206b86d9711abc50fdafaf7a2104847aef2e55c
Subins11 <122017529+Subins11@users.noreply.github.com>
2023-11-13 18:09:40 +0530

    new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-demo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-demo-app');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-demo-app';
-  intern = 'FE Internship Session'
+
+  example () {
+
+    let var1 = 'Hello';
+    var var2 = 'Beinex';
+    const var3 = 'Team';
+
+    if(2>1){
+      let var1 = 'Good';
+      var var2 = 'Morning';
+
+      console.log(var1)
+      console.log(var2)
+      console.log(var3)
+    }
+  }
 }
+
+// Let Example  Only Accesible within the block
+
+let count = 6;
+if (count > 0){
+  let message = 'Positive Count';
+  console.log('Message:', message)
+}
+
+// Var Example : It can be Accesible outside the Scope
+var number = 6;
+if (number > 0){
+  let message = 'Positive Count';
+  console.log('message:', number)
+}
+console.log('number:', number);
+console.log('message2',number )
+
+//Const Example: Can be modified but cannot be re-assigned
+
+const person = { name: 'Subin', age :26}
+
+person.age=28;
+console.log('Person', person)
+
+
+
